c5a60c76ed0e2f4c14bec736630470fc07b2d99d
Sahel_Cavalieri <sahelblender3d@gmail.com>
2026-09-15 09:21:11 -0300

    Add simple Hello World program in C++

---
+++ src/hello_world_format_english.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// @author (Sahel_Cavalieri)
+// @date 09:17 09/15/2026
+// #brief Simple Hello World (Format English)
+// Information about total Lines:  17
+// Version: 1.0
+// Website: https://mundopauta.com.br/ (Portuguese, Brazil)
+
+int main()
+{
+	
+	std::cout << "Hello World";
+
+	return 0;
+}
