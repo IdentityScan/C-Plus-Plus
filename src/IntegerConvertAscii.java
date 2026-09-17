b9c2da4006c599bd87dc0bee96e7c81bb548496c
Sahel_Cavalieri <sahelblender3d@gmail.com>
2026-09-17 07:41:18 -0300

    Update and rename integer_convert_to_ascii.cpp to IntegerConvertAscii.java

---
+++ src/IntegerConvertAscii.java
@@ -0,0 +1,9 @@
+public class IntegerConvertAscii
+{
+	public static void main (String[]args)
+	{
+		int indexascii = 65;
+		char symbol = (char) indexascii;
+		System.out.println("The value " + indexascii + " show from ASCII Table is " + symbol);
+	}
+}
