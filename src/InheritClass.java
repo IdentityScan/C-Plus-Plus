b0f67ee29bc13cd68b8c07774fa9c565c183fb0f
Sahel_Cavalieri <sahelblender3d@gmail.com>
2026-09-17 07:30:52 -0300

    Update and rename class_inherit.cpp to InheritClass.java

---
+++ src/InheritClass.java
@@ -0,0 +1,13 @@
+class Example
+{
+	public static String name = "Example class";
+}
+
+
+public class InheriClass extends Example
+{
+	public static void main (String[]args)
+	{
+		System.out.println(name);
+	}
+}
