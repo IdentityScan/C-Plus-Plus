d2be622a30422d17a05e2e08961f8bfb900fccbc
Sahel_Cavalieri <sahelblender3d@gmail.com>
2026-09-17 07:29:10 -0300

    Rename App1 class to ExampleClass

---
+++ src/ExampleClass.java
@@ -0,0 +1,14 @@
+class Example
+{
+	public String name = "Example class";
+}
+
+
+public class ExampleClass
+{
+	public static void main (String[]args)
+	{
+		Example ex = new Example();
+		System.out.println(ex.name);
+	}
+}
