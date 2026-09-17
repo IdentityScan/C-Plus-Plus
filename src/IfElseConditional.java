a77d3be694bb94179ef2366a4650b89766256f9d
Sahel_Cavalieri <sahelblender3d@gmail.com>
2026-09-17 07:38:32 -0300

    Update and rename if_else_conditional.cpp to IfElseConditional.java

---
+++ src/IfElseConditional.java
@@ -0,0 +1,15 @@
+public class IfElseConditional
+{
+	public static void main (String[]args)
+	{
+		int valuea = 5, valueb = 7;
+
+		if (valuea > valueb)
+		{
+			System.out.println(valuea + " is major then " + valueb);
+		} else
+		{
+			System.out.println(valuea + " is minor then " + valueb);
+		}
+	}
+}
