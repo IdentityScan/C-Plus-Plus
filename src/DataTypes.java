bfb9c9420068ac2a73b1c14762263c28b2d7b406
Sahel_Cavalieri <sahelblender3d@gmail.com>
2026-09-17 07:17:50 -0300

    Rename class App1 to DataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes
+{
+	public static void main(String[]args)
+	{
+		String name = "Aloy";
+		int age = 19;
+		char title = 'H';
+		float behavior = 1.5f;
+		boolean flagState = true;
+
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Title's char: " + title);
+		System.out.println("Variable float: " + behavior);
+		System.out.println("State Aloy: " + flagState);
+	}
+}
