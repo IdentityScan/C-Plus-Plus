203afd0b6c2003bd144981861c965b9532401dc3
Sahel_Cavalieri <sahelblender3d@gmail.com>
2026-09-17 07:55:29 -0300

    Update and rename array_sizeof.cpp to ArraySize.java

---
+++ src/ArraySize.java
@@ -0,0 +1,14 @@
+public class ArraySize
+{
+	public static void main (String[]args)
+	{
+		// int[] grade = new int[5]
+		int[] grade = {1,2,3,4,5};
+		int sizeArray = grade.length;
+		
+		System.out.println("Index 0 is " + grade[0]);
+		System.out.println("Index 4 is " + grade[4]);
+
+		System.out.println("Grade's Size " + sizeArray);
+	}
+}
